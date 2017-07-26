fe0aec9137122389b76a1ee4efdae6b191880454
y4izus <yaiza.garcia.mm@gmail.com>
2017-07-26 12:33:50 +0200

    Movement to right and left completed

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,40 @@
 // Rover Object Goes Here
 // ======================
-
+var rover = {
+  direction: "N"
+};
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  calculateNewPosition(rover.direction, "left");
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  calculateNewPosition(rover.direction, "right");
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  console.log("moveForward was called");
+}
+
+function calculateNewPosition(currentDirection, turnTo){
+  switch (currentDirection) {
+    case "N":
+      if (turnTo === "right") rover.direction = "E";
+      if (turnTo === "left") rover.direction = "W";
+      break;
+    case "W":
+      if (turnTo === "right") rover.direction = "N";
+      if (turnTo === "left") rover.direction = "S";
+      break;
+    case "S":
+      if (turnTo === "right") rover.direction = "W";
+      if (turnTo === "left") rover.direction = "E";
+      break;
+    case "E":
+      if (turnTo === "right") rover.direction = "S";
+      if (turnTo === "left") rover.direction = "N";
+      break;
+  }
 }
