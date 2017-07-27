8de96ff691cb4890eb9e9b7307d4bd89038af7dd
y4izus <yaiza.garcia.mm@gmail.com>
2017-07-27 14:18:38 +0200

    Enforce Boundaries

--- javascripts/app.js
+++ javascripts/app.js
@@ -26,16 +26,23 @@ function moveForward(rover){
   if(rover.direction === "S") rover.y ++;
   if(rover.direction === "E") rover.x ++;
 
+  var hasMoved = true;
   if(rover.x===-1 || rover.y ===-1){
     console.log("Sorry, Rover can't go out of boundaries");
+    hasMoved = false;
     if (rover.x===-1) rover.x ++;
     if (rover.y===-1) rover.y ++;
   }
 
   rover.travelLog.push("[" + rover.x + "," + rover.y + "]");
 
-  console.log("Rover is moving to " + rover.direction + ". Next position: [" +
-   rover.x + "," + rover.y + "]");
+  if (hasMoved) {
+    console.log("Rover is moving to " + rover.direction + ". Next position: [" +
+     rover.x + "," + rover.y + "]");
+  } else {
+     console.log("Rover is not moving to " + rover.direction + ". Stays in: [" +
+      rover.x + "," + rover.y + "]");
+  }
 }
 
 function calculateNewDirection(currentDirection, turnTo){
