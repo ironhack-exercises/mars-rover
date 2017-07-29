26bcde3f2eea5c1510367373153f686d1318ac80
y4izus <yaiza.garcia.mm@gmail.com>
2017-07-29 20:23:06 +0200

    Move backward and obstacles

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,6 +6,20 @@ var rover = {
   y: 0,
   travelLog: ["[0,0]"]
 };
+
+var grid = [
+  [null, "Potato", null, "Potato", null, null, null, null, null, null],
+  [null, "Potato", null, null, null, null, null, null, null, null],
+  ["Matt Damon", null, null, null, null, null, null, null, "Potato", null],
+  [null, "Potato", null, null, null, null, null, null, null, null],
+  [null, null, null, null, "Potato", null, null, null, null, null],
+  [null, null, null, "Potato", null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, "Potato"],
+  ["Potato", null, null, null, null, null, null, null, null, null],
+  [null, "Potato", null, null, null, null, null, "Potato", null, null],
+  [null, null, null, null, "Potato", null, null, null, null, null]
+];
+
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
@@ -19,24 +33,68 @@ function turnRight(rover){
 
 function moveForward(rover){
   console.log("moveForward was called");
+  var hasMoved = makeMovement (rover.direction, "forward");
+  registerMovement(rover, hasMoved);
+}
+
+function moveBackward(rover){
+  console.log("moveBackward was called");
+  var hasMoved = makeMovement (rover.direction, "backward");
+  registerMovement(rover, hasMoved);
+}
+
+function makeMovement (currentDirection, movement){
   console.log("Rover current position: [" + rover.x + "," + rover.y + "]");
-
-  if(rover.direction === "N") rover.y --;
-  if(rover.direction === "W") rover.x --;
-  if(rover.direction === "S") rover.y ++;
-  if(rover.direction === "E") rover.x ++;
-
   var hasMoved = true;
-  if(rover.x===-1 || rover.y ===-1){
+  switch (currentDirection) {
+    case "N":
+      if (movement === "forward")
+        hasMoved = checkMovement(rover.x - 1, rover.y);
+      if (movement === "backward")
+        hasMoved = checkMovement(rover.x + 1, rover.y);
+      break;
+    case "W":
+      if (movement === "forward")
+        hasMoved = checkMovement(rover.x, rover.y - 1);
+      if (movement === "backward")
+        hasMoved = checkMovement(rover.x, rover.y + 1);
+      break;
+    case "S":
+      if (movement === "forward")
+        hasMoved = checkMovement(rover.x + 1, rover.y);
+      if (movement === "backward")
+        hasMoved = checkMovement(rover.x - 1, rover.y);
+      break;
+    case "E":
+      if (movement === "forward")
+        hasMoved = checkMovement(rover.x, rover.y + 1);
+      if (movement === "backward")
+        hasMoved = checkMovement(rover.x, rover.y - 1);
+      break;
+  }
+  return hasMoved;
+}
+
+function checkMovement (roverNextX, roverNextY){
+  var hasMoved = true;
+  if(roverNextX === -1 || roverNextY === -1){
     console.log("Sorry, Rover can't go out of boundaries");
     hasMoved = false;
-    if (rover.x===-1) rover.x ++;
-    if (rover.y===-1) rover.y ++;
+  } else {
+    if(grid[roverNextX][roverNextY] === null) {
+      rover.x = roverNextX;
+      rover.y = roverNextY;
+    }else{
+      console.log("\u26A0 Obstacle found \u26A0 => " + grid[roverNextX][roverNextY]);
+      hasMoved = false;
+    }
   }
+  return hasMoved;
+}
 
-  rover.travelLog.push("[" + rover.x + "," + rover.y + "]");
-
+function registerMovement(rover, hasMoved){
   if (hasMoved) {
+    rover.travelLog.push("[" + rover.x + "," + rover.y + "]");
     console.log("Rover is moving to " + rover.direction + ". Next position: [" +
      rover.x + "," + rover.y + "]");
   } else {
@@ -49,31 +107,41 @@ function calculateNewDirection(currentDirection, turnTo){
   switch (currentDirection) {
     case "N":
       if (turnTo === "right") rover.direction = "E";
-      if (turnTo === "left") rover.direction = "W";
+      else if (turnTo === "left") rover.direction = "W";
       break;
     case "W":
       if (turnTo === "right") rover.direction = "N";
-      if (turnTo === "left") rover.direction = "S";
+      else if (turnTo === "left") rover.direction = "S";
       break;
     case "S":
       if (turnTo === "right") rover.direction = "W";
-      if (turnTo === "left") rover.direction = "E";
+      else if (turnTo === "left") rover.direction = "E";
       break;
     case "E":
       if (turnTo === "right") rover.direction = "S";
-      if (turnTo === "left") rover.direction = "N";
+      else if (turnTo === "left") rover.direction = "N";
       break;
   }
 }
 
 function execListOfCommands (commands) {
   for (var i=0; i<commands.length; i++){
-    if (commands[i] !== "f" && commands[i] !== "r" && commands[i] !== "l")
-      console.log("Command NOT correct, you can only use 'f', 'r' and 'l'");
-    else {
-      if (commands[i] === "f") moveForward(rover);
-      if (commands[i] === "r") turnRight(rover);
-      if (commands[i] === "l") turnLeft(rover);
+    switch (commands[i]) {
+      case "f":
+        moveForward(rover);
+        break;
+      case "b":
+        moveBackward(rover);
+        break;
+      case "l":
+        turnLeft(rover);
+        break;
+      case "r":
+        turnRight(rover);
+        break;
+      default:
+        console.log("Command NOT correct, you can only use 'f', 'b', 'r' and 'l'");
+        break;
     }
   }
 
@@ -82,7 +150,7 @@ function execListOfCommands (commands) {
 
 function printMovements(){
   console.log("Travel Log: \n");
-  for(var move in rover.travelLog){
-    console.log(rover.travelLog[move]);
+  for(var movement in rover.travelLog){
+    console.log(rover.travelLog[movement]);
   }
 }
